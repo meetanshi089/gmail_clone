4def7419665ff0bd134c74e694fbb3a35313dacb
meetanshi089 <meetanshitomar@gmail.com>
2025-06-03 01:09:05 +0530

    sideicons css

--- src/components/layouts/EmailsView.js
+++ src/components/layouts/EmailsView.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./EmailsView.css";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import Checkbox from "@mui/material/Checkbox";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { IconButton } from "@mui/material";
 function EmailsView() {
   return (
-    <div>
-      <p>Emails hegjhghjwbejbasdhb</p>
+    <div className="emailview">
+      <div className="topicons">
+        <Checkbox
+          icon={<CheckBoxOutlineBlankIcon />}
+          checkedIcon={<CheckBoxIcon />}
+          onChange={(e) => console.log("Checked:", e.target.checked)}
+        />
+        <IconButton>
+          <RefreshIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      </div>
+      <div className="emailcontainer"></div>
     </div>
   );
 }

921db2e2dc12d8f0dc5a05a98344510f5e876643
meetanshi089 <meetanshitomar@gmail.com>
2025-06-03 23:33:09 +0530

    emailsview

--- src/components/Emailitem/EmailItem.js
+++ src/components/Emailitem/EmailItem.js
@@ -1,8 +1,32 @@
-import React from "react";
-const EmailItem = (
-  { starred, from, subject, message, received, read },
-  index
-) => {
-  return <div>email</div>;
+import React, { useState } from "react";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import Checkbox from "@mui/material/Checkbox";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import { IconButton } from "@mui/material";
+import "./Emailitem.css";
+const EmailItem = ({ starred, from, subject, message, received, read }) => {
+  const [star, setStar] = useState(starred);
+  const changeStar = () => {};
+  return (
+    <div className="emailitemwrap">
+      <Checkbox
+        icon={<CheckBoxOutlineBlankIcon />}
+        checkedIcon={<CheckBoxIcon />}
+        onChange={(e) => console.log("Checked:", e.target.checked)}
+      />
+      <IconButton onClick={() => (star ? setStar(false) : setStar(true))}>
+        {star ? <StarIcon htmlColor="#f7cb69" /> : <StarBorderIcon />}
+      </IconButton>
+      <div className="emailbody">
+        <p className={!read && "unread"}>{from}</p>
+
+        <p className={!read && "unread"}>{subject}</p>
+
+        <p className={!read && "unread"}>{received}</p>
+      </div>
+    </div>
+  );
 };
 export default EmailItem;
